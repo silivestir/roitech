3432ee3f2021cb53cd39e02783d80041f58b878c
silivestir <84616833+silivestir@users.noreply.github.com>
2025-02-10 16:52:45 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,19 +1,10 @@
-require("dotenv").config()
-
-
-//db username:
-//host:
-//port:
-//user:
-//password:
-
-
 module.exports = {
     development: {
         username: 'avnadmin',
         password: 'AVNS_rMwDgY-tb25PHzJxfUk',
         database: 'defaultdb',
         host: 'pg-192dc673-silvestiriassey.k.aivencloud.com',
-        port: 10140
+        port: 10140,
+        ssl: { rejectUnauthorized: false } // optional for SSL connection depending on setup
     }
-}
+};
