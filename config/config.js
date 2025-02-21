5ed1fcaca29201aff5b3b82bf1adc7b3b34d48c6
silivestir <84616833+silivestir@users.noreply.github.com>
2025-02-21 17:53:55 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     development: {
-        username: 'avnadmin',
-        password: 'AVNS_rMwDgY-tb25PHzJxfUk',
-        database: 'defaultdb',
-        host: 'pg-192dc673-silvestiriassey.k.aivencloud.com',
-        port: 10140,
-        ssl: { rejectUnauthorized: false } // optional for SSL connection depending on setup
+        username: process.env.username,  // Fetches from environment variable
+        password: process.env.password,  // Fetches from environment variable
+        database: process.env.database, // Fetches from environment variable
+        host: process.env.host,         // Fetches from environment variable
+        port: process.env.port,         // Fetches from environment variable
+        ssl: { rejectUnauthorized: false }  // optional for SSL connection depending on setup
     }
 };
